122a1edcdccdd6ce7ee3aa36dcf49bac85d4cba2
ivandaho <ivandaho@gmail.com>
2016-08-07 19:34:26 +0800

    frontend/css overhaul
    changed the bootstrap package to one that compiles beforehand
    this allows variable changing
    
    fixed viewport not defined for moble

--- imports/ui/layouts/Header.js
+++ imports/ui/layouts/Header.js
@@ -16,11 +16,6 @@ Template.Header.helpers({
 });
 
 Template.Header.events({
-    //'click .account-info' (event) {
-    'click .btn-booknow' (event) {
-        event.preventDefault();
-        FlowRouter.go('schedule');
-    },
     'click .test-btn' (event) {
         event.preventDefault();
         bootbox.alert('test');
